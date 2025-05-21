b539e32c13a1c24db968e91fff7e9782108fb642
Kulik Kateryna <katerynakulik4@gmail.com>
2025-05-21 22:39:37 +0300

    1 lesson

---
+++ js/lesson-1.js
@@ -0,0 +1,115 @@
+// function Determine() {
+//     const min = Math.floor(Math.random() * (59 - 0) + 0);
+//     if (min > 45) {
+//         alert (`${min} входить в четверту чверть`);
+//     } else if (min > 30) {
+//         alert (`${min} входить в третю чверть`);
+//     } else if (min > 15) {
+//         alert (`${min} входить в другу чверть`);
+//     } else {
+//         alert (`${min} входить в першу чверть`);
+//     }
+
+// };
+// Determine();
+
+// function input() {
+//     const num = prompt('Введіть число від 1 до 4');
+//     let result;
+//     switch (Number(num)) {
+//         case 1:
+//             result = "зима";
+//             break;
+//         case 2:
+//             result = "весна";
+//             break;
+//         case 3:
+//             result = "літо";
+//             break;
+//         case 4:
+//             result = "осінь";
+//             break;
+//         default:
+//             result = 'Вибачте, але ви маєте ввести значення від 1 до 4 включно'
+//             break;
+//     }
+//     return result;
+// };
+
+// console.log(input());
+
+
+
+// function determineTime() {
+//   const zeit = Number(prompt("Введіть кількість хвилин"));
+//   let hour = Math.floor(zeit / 60);
+//   let minute = zeit % 60;
+//   const formattedHour = String(hour).padStart(2, "0");
+//   const formattedMin = String(minute).padStart(2, "0");
+//   alert(`${formattedHour}:${formattedMin}`);
+// }
+// determineTime();
+
+
+// function identification() {
+//     const login = prompt('Введіть логін');
+//     if (login =='Адмін') {
+//         const password = prompt('Введіть пароль');
+//         if (password==null || password==''){
+//             alert('Скасовано');
+//         } else if (password == 'Я головний'){
+//             alert('Добрий день!');
+//         } else {
+//             alert('Невірний пароль!');
+//         }
+        
+//     } else {
+//         alert('Я Вас не знаю');
+//     }
+//         return;
+// };
+
+// console.log(identification());
+
+// function counting(){
+//     let count = 0;
+//     while (count<=20){
+//         console.log(count);
+//         count+=1;
+//     }
+// return;
+// };
+// counting();
+
+function getNumbers(min, max) {
+    let newcount = 0;
+    for (let i=max; i>=min; i--){
+        console.log(i);
+        if (i % 2 === 0){
+            newcount += i;
+        }
+    }
+    return newcount;
+    
+};
+
+console.log(getNumbers(3,15));
+
+
+
+
+function min(a, b) {
+    if (a === Number(a) && b === Number(b)){
+        if (a > b) {
+            return b;
+        } else {
+            return a;
+        }
+    } else {
+        return 'Not a number!';
+    }
+};
+
+console.log(min(15,27));
+console.log(min(215,27));
+console.log(min(15,'27'));
